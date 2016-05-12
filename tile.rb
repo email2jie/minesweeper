b6a0b36a32a817c9a9b5c0587fd13cd18a35e002
App Academy Student <student@appacademy.io>
2016-05-12 15:51:00 -0700

    filled in tile.rb

--- tile.rb
+++ tile.rb
@@ -1,4 +1,27 @@
 require 'byebug'
 class Tile
-  
+  attr_reader :bomb, :flag, :checked, :revealed
+
+  def initialize(bomb=false, flag=false, checked=0, revealed = false)
+    @bomb = bomb
+    @flag = flag
+    @checked = 0
+    @revealed = revealed
+  end
+
+  def set_bomb
+    @bomb = true
+  end
+
+  def change_flag
+    @flag == false ? @flag = true : @flag = false
+  end
+
+  def inc_checked
+    checked+=1
+  end
+
+  def reveal
+    @revealed = true
+  end
 end
